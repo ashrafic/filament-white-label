ba398cf4a0cecb8ab2552f11e85f94373d03c985
Ashraful Islam <25670420+ashraful19@users.noreply.github.com>
2026-06-25 06:21:48 +0600

    feat: make font default suffix translatable

--- src/Fonts/FontService.php
+++ src/Fonts/FontService.php
@@ -9,7 +9,7 @@ class FontService
     public static function fontOptions(): array
     {
         return [
-            'Inter' => 'Inter (Default)',
+            'Inter' => 'Inter '.__('filament-white-label::filament-white-label.fonts.default_suffix'),
             'Roboto' => 'Roboto',
             'Open Sans' => 'Open Sans',
             'Lato' => 'Lato',
